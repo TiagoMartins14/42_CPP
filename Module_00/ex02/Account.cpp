19342187d2fb9eb4f9bf877784ab085971852043
tiaferna <tiaferna@student.42porto.com>
2024-06-18 18:58:50 +0100

    first commit

--- Module_00/ex02/Account.cpp
+++ Module_00/ex02/Account.cpp
@@ -5,8 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tiaferna <tiaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/14 15:11:59 by tiaferna          #+#    #+#             */
-/*   Updated: 2024/06/14 15:12:38 by tiaferna         ###   ########.fr       */
+/*   Created: 2024/06/18 18:34:59 by tiaferna          #+#    #+#             */
+/*   Updated: 2024/06/18 18:51:09 by tiaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Account.hpp"
+
+int Account::getNbAccounts(void) { return Account::_nbAccounts; }
+
+int Account::getTotalAmount(void) { return Account::_totalAmount; }
+
+int Account::getNbDeposits(void) { return Account::_totalNbDeposits; }
+
+int Account::getNbWithdrawals(void) { return Account::_totalNbWithdrawals; }
 
