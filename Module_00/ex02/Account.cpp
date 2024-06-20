7c9f7103b33826e045a617f3a1b3732f54d0d3f1
tiaferna <tiaferna@student.42porto.com>
2024-06-20 22:03:40 +0100

    added timestamp function

--- Module_00/ex02/Account.cpp
+++ Module_00/ex02/Account.cpp
@@ -6,11 +6,28 @@
 /*   By: tiaferna <tiaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 18:34:59 by tiaferna          #+#    #+#             */
-/*   Updated: 2024/06/18 18:51:09 by tiaferna         ###   ########.fr       */
+/*   Updated: 2024/06/20 22:03:03 by tiaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+
+void	printTimeStamp()
+{
+	std::time_t now = std::time(0);
+
+	std::tm* localTime = std::localtime(&now);
+	
+	std::cout << "[" << (localTime->tm_year + 1900)
+			  << std::setw(2) << std::setfill('0') << (localTime->tm_mon + 1)
+			  << std::setw(2) << std::setfill('0') << localTime->tm_mday << "_"
+			  << std::setw(2) << std::setfill('0') << localTime->tm_hour
+			  << std::setw(2) << std::setfill('0') << localTime->tm_min
+			  << std::setw(2) << std::setfill('0') << localTime->tm_sec << "] "
+}
 
 int Account::getNbAccounts(void) { return Account::_nbAccounts; }
 
@@ -20,3 +37,17 @@ int Account::getNbDeposits(void) { return Account::_totalNbDeposits; }
 
 int Account::getNbWithdrawals(void) { return Account::_totalNbWithdrawals; }
 
+void	Account::displayAccountsInfos(void)
+{
+	printTimeStamp();
+
+	std::cout << "accounts:" << Account::getNbAccounts()
+			  << ";total:" << Account::getTotalAmount()
+			  << ";deposits:" << Account::getNbDeposits()
+			  << ";withdrawals:" << Account::getNbWithdrawals() << std::endl;
+}
+
+Account::Account(int initial_deposit)
+{
+	printTimeStamp();
+}
