38ceba8df5b62e83b2f0a22375b5e0db4622f9ae
tiaferna <tiaferna@student.42porto.com>
2024-07-04 23:22:52 +0100

    created Fixed hpp

---
+++ Module_02/ex01/Fixed.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiaferna <tiaferna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/04 22:58:27 by tiaferna          #+#    #+#             */
+/*   Updated: 2024/07/04 23:19:52 by tiaferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+
+class Fixed
+{
+private:
+
+	int					_fpValue;
+	static const int	_numOfFractionalBits = 8;
+
+public:
+
+	Fixed ();
+	Fixed(const Fixed& other);
+	Fixed(const int integer);
+	Fixed(const float floatingPoint);
+	Fixed&	operator=(const Fixed& other);
+	friend std::ostream &operator<<(std::ostream& out, const Fixed &fixed);
+	~Fixed();
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+	float	toFloat(void) const;
+	int		toInt(void) const;
+};
