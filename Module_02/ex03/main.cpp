0ff92a5fd0aeb58de8db075b72333ca38d20c73a
tiaferna <tiaferna@student.42.fr>
2024-07-09 15:39:45 +0100

    erased old main content

--- Module_02/ex03/main.cpp
+++ Module_02/ex03/main.cpp
@@ -6,27 +6,14 @@
 /*   By: tiaferna <tiaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 17:21:48 by tiaferna          #+#    #+#             */
-/*   Updated: 2024/07/07 14:31:48 by tiaferna         ###   ########.fr       */
+/*   Updated: 2024/07/09 15:14:05 by tiaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Fixed.hpp"
 
-int main( void )
+int main(int argc, char **argv)
 {
-	Fixed 		a;
-	Fixed const	b( Fixed( 5.05f ) * Fixed( 2 ) );
-
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-
-	std::cout << b << std::endl;
-
-	std::cout << Fixed::max( a, b ) << std::endl;
-
-	return 0;
+	
 }
