f82840a3db8c678716f2a5849d6fb8ca85fd2ad8
tiaferna <tiaferna@student.42porto.com>
2024-07-16 10:27:28 +0100

    created FragTrap

---
+++ Module_03/ex02/FragTrap.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiago <tiago@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 10:01:10 by tiago             #+#    #+#             */
+/*   Updated: 2024/07/16 10:17:55 by tiago            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include "ScavTrap.hpp"
+
+class FragTrap : public ScavTrap
+{
+public:
+	FragTrap();
+	FragTrap(const std::string &Name);
+	FragTrap(const FragTrap &other);
+	FragTrap();
+	FragTrap &operator=(const FragTrap &other);
+
+	void highFivesGuys(void);
+	void attack(const std::string &target);
+
+	friend std::ostream &operator<<(std::ostream &out, const FragTrap &scavTrap);
+};
---
+++ Module_03/ex02/ScavTrap.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiago <tiago@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/14 13:30:22 by tiago             #+#    #+#             */
+/*   Updated: 2024/07/16 10:04:09 by tiago            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+public:
+	ScavTrap();
+	ScavTrap(const std::string &Name);
+	ScavTrap(const ScavTrap &other);
+	~ScavTrap();
+	ScavTrap &operator=(const ScavTrap &other);
+
+	void guardGate();
+	void attack(const std::string &target);
+
+	friend std::ostream &operator<<(std::ostream &out, const ScavTrap &scavTrap);
+};
