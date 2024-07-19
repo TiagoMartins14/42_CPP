27fdb674bd5733d2f01fc0e152d8ec06a6983d6d
tiaferna <tiaferna@student.42.fr>
2024-07-19 14:50:58 +0100

    added protection against memory allocation error

--- Module_01/ex01/zombieHorde.cpp
+++ Module_01/ex01/zombieHorde.cpp
@@ -6,18 +6,27 @@
 /*   By: tiaferna <tiaferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 17:23:11 by tiaferna          #+#    #+#             */
-/*   Updated: 2024/06/25 17:59:57 by tiaferna         ###   ########.fr       */
+/*   Updated: 2024/07/17 17:23:12 by tiaferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie*	zombieHorde( int N, std::string name )
+Zombie *zombieHorde(int N, std::string name)
 {
-	Zombie* horde = new Zombie[N];
+	Zombie *horde;
+	try
+	{
+		horde = new Zombie[N];
+	}
+	catch (std::bad_alloc &e)
+	{
+		std::cerr << "ERROR: " << e.what() << std::endl;
+		return (NULL);
+	}
 
 	for (int i = 0; i < N; i++)
 		new (&horde[i]) Zombie(name);
-	
+
 	return horde;
 }
