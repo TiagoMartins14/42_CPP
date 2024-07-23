def94211f6d5075cc31a45718d750f9bf04679d5
tiaferna <tiaferna@student.42porto.com>
2024-07-23 14:08:13 +0100

    no leaks during tests

--- Module_04/ex01/main.cpp
+++ Module_04/ex01/main.cpp
@@ -3,43 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tiaferna <tiaferna@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tiago <tiago@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 16:24:51 by tiaferna          #+#    #+#             */
-/*   Updated: 2024/07/19 16:57:42 by tiaferna         ###   ########.fr       */
+/*   Updated: 2024/07/23 11:20:57 by tiago            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
-#include "WrongAnimal.hpp"
 #include "Cat.hpp"
-#include "WrongCat.hpp"
 #include "Dog.hpp"
 
 int main()
 {
-	const Animal *meta = new Animal();
-	const Animal *j = new Dog();
-	const Animal *i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); // will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
-	delete meta;
-	delete j;
-	delete i;
+	const int SIZE = 6;
+	Animal *animals[SIZE];
 
-	std::cout << std::endl;
-	std::cout << "-------------- Wrong Animal -------------" << std::endl;
-	std::cout << std::endl;
+	std::cout << "------ Creating cats and dogs with brains classes -----" << std::endl
+			  << std::endl;
+	for (size_t i = 0; i < SIZE / 2; i++)
+		animals[i] = new Dog;
+	for (size_t i = SIZE / 2; i < SIZE; i++)
+		animals[i] = new Cat;
 
-	const WrongAnimal *meta2 = new WrongAnimal();
-	const WrongAnimal *l = new WrongCat();
-	std::cout << l->getType() << " " << std::endl;
-	l->makeSound(); // will output the animal sound!
-	meta2->makeSound();
-	delete meta2;
-	delete l;
-	return 0;
+	std::cout << std::endl
+			  << "------ Destroying created classes -----" << std::endl
+			  << std::endl;
+	for (int i = 0; i < SIZE; i++)
+		delete animals[i];
 }
