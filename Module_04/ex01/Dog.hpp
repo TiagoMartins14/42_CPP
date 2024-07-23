23644bb1ad3aafe628f209061a49fbb1f18c6a94
tiaferna <tiaferna@student.42porto.com>
2024-07-23 14:29:56 +0100

    clean up

--- Module_04/ex01/Dog.hpp
+++ Module_04/ex01/Dog.hpp
@@ -3,18 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   Dog.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tiaferna <tiaferna@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tiago <tiago@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 15:34:47 by tiaferna          #+#    #+#             */
-/*   Updated: 2024/07/19 15:22:07 by tiaferna         ###   ########.fr       */
+/*   Updated: 2024/07/22 18:55:09 by tiago            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include "Animal.hpp"
+#include "Brain.hpp"
 
 class Dog : public Animal
 {
+private:
+	Brain *_brain;
+
 public:
 	Dog();
 	Dog(const Dog &copy);
