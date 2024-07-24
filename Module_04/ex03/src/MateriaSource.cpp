dbb244dd7b05b37360f828bd7778aa5722d1e689
tiaferna <tiaferna@student.42porto.com>
2024-07-25 00:08:35 +0100

    implemented member functions

---
+++ Module_04/ex03/src/MateriaSource.cpp
@@ -0,0 +1,66 @@
+#include "MateriaSource.hpp"
+
+MateriaSource::MateriaSource()
+{
+	for (int idx = 0; idx < 4; idx++)
+		this->_materias[idx] = NULL;
+}
+
+MateriaSource::MateriaSource(const MateriaSource &other)
+{
+	for (int idx = 0; idx < 4; idx++)
+		this->_materias[idx] = NULL;
+
+	*this = other;
+}
+
+MateriaSource &MateriaSource::operator=(const MateriaSource &copy)
+{
+	if (this != &copy)
+	{
+		for (int idx = 0; idx < 4; idx++)
+		{
+			if (this->_materias)
+				delete this->_materias;
+			if (copy._materias[idx])
+				this->_materias[idx] = copy._materias[idx]->clone();
+			else
+				this->_materias[idx] = NULL;
+		}
+	}
+	return *this;
+}
+
+MateriaSource::~MateriaSource()
+{
+	for (int idx = 0; idx < 4; idx++)
+	{
+		if (this->_materias[idx])
+			delete this->_materias[idx];
+	}
+}
+
+void MateriaSource::learnMateria(AMateria *materia)
+{
+	int idx = 0;
+
+	while (this->_materias[idx] && idx < 4)
+		idx++;
+	if (idx < 4)
+		this->_materias[idx] = materia;
+	else
+		std::cout << "No space left to learn a new materia!" << std::endl;
+}
+
+AMateria *MateriaSource::createMateria(std::string const &type)
+{
+	int idx = 0;
+	while (this->_materias[idx] && this->_materias[idx]->getType() != type && idx < 4)
+		idx++;
+	if (idx == 4)
+	{
+		std::cout << "Unable to create Materia. Type not learnt." << std::endl;
+		return NULL;
+	}
+	return this->_materias[idx]->clone();
+}
