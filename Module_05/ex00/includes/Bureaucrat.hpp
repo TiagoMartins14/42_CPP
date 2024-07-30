625582a514a05d1927d7e9e2a08e65dfeea9ed49
tiaferna <tiaferna@student.42.fr>
2024-07-30 13:38:59 +0100

    updated exceptions handling

---
+++ Module_05/ex00/includes/Bureaucrat.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
+#define MAX_GRADE 1
+#define MIN_GRADE 150
+
+class Bureaucrat
+{
+private:
+	const	std::string _name;
+	int	_grade;
+public:
+	Bureaucrat();
+	Bureaucrat(std::string name, int grade);
+	Bureaucrat(const Bureaucrat &other);
+	~Bureaucrat();
+	Bureaucrat &operator=(const Bureaucrat &copy);
+
+	std::string	getName();
+	int		getGrade();
+
+	void		incrementGrade();
+	void		decrementGrade();
+
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		const char *what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		const char *what() const throw();
+	};
+
+	friend std::ostream &operator<<(std::ostream &out, const Bureaucrat &bureaucrat);
+};
