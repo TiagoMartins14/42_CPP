883a067d5e2a163b94dba5de3a6f73752d05d911
Tiago Martins <tiago86martins@gmail.com>
2024-08-01 15:15:21 +0100

    initial commit

---
+++ Module_05/ex01/src/Bureaucrat.cpp
@@ -0,0 +1,84 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat () : _name ("no name"), _grade (150) {}
+
+Bureaucrat::Bureaucrat (std::string name, int grade) : _name (name)
+{
+	if (grade < MAX_GRADE)
+		{
+			throw GradeTooHighException ();
+		}
+	else if (grade > MIN_GRADE)
+		{
+			throw GradeTooLowException ();
+		}
+	_grade = grade;
+}
+
+Bureaucrat::Bureaucrat (const Bureaucrat &other) { *this = other; }
+
+Bureaucrat::~Bureaucrat () {}
+
+Bureaucrat &
+Bureaucrat::operator= (const Bureaucrat &copy)
+{
+	if (this != &copy)
+		{
+			this->_name = copy._name;
+			this->_grade = copy._grade;
+		}
+	return *this;
+}
+
+std::string
+Bureaucrat::getName ()
+{
+	return _name;
+}
+
+int
+Bureaucrat::getGrade ()
+{
+	return _grade;
+}
+
+void
+Bureaucrat::incrementGrade ()
+{
+	if (_grade - 1 < MAX_GRADE)
+		{
+			throw GradeTooHighException ();
+		}
+	_grade--;
+}
+
+void
+Bureaucrat::decrementGrade ()
+{
+	if (_grade + 1 > MIN_GRADE)
+		{
+			throw GradeTooLowException ();
+		}
+	_grade++;
+}
+
+const char *
+Bureaucrat::GradeTooHighException::what () const throw ()
+{
+	return "Grade too high";
+}
+
+const char *
+Bureaucrat::GradeTooLowException::what () const throw ()
+{
+	return "Grade too low";
+}
+
+std::ostream &
+operator<< (std::ostream &out, Bureaucrat &bureaucrat)
+{
+	out << bureaucrat.getName () << ", bureaucrat grade "
+		<< bureaucrat.getGrade () << "." << std::endl;
+
+	return out;
+}
---
+++ Module_05/ex01/src/Form.cpp
@@ -0,0 +1,74 @@
+#include "Form.hpp"
+
+Form::Form ()
+	: _name ("no name"), _gradeToSign (1), _gradeToExecute (1), _signed (false)
+{
+}
+
+Form::Form (const std::string name, const int gradeToSign,
+			const int gradeToExecute)
+	: _name (name), _gradeToSign (gradeToSign),
+	  _gradeToExecute (gradeToExecute), _signed (false)
+{
+}
+
+Form::Form (const Form &other)
+	: _name (other._name), _gradeToSign (other._gradeToSign),
+	  _gradeToExecute (other._gradeToExecute), _signed (other._signed)
+{
+}
+
+Form::~Form () {}
+
+Form &
+Form::operator= (const Form &copy)
+{
+	if (this != &copy)
+		this->_signed = copy._signed;
+	return *this;
+}
+
+const std::string
+Form::getName ()
+{
+	return _name;
+}
+
+int
+Form::getGradeToSign ()
+{
+	return _gradeToSign;
+}
+
+int
+Form::getGradeToExecute ()
+{
+	return _gradeToExecute;
+}
+
+bool
+Form::getSigned ()
+{
+	return _signed;
+}
+
+void
+Form::beSigned (Bureaucrat &bureaucrat)
+{
+	if (bureaucrat.getGrade () <= this->getGradeToSign ())
+		this->_signed = true;
+	else
+		GradeTooLowException ();
+}
+
+const char *
+Form::GradeTooHighException::what () const throw ()
+{
+	return "Grade too high";
+}
+
+const char *
+Form::GradeTooLowException::what () const throw ()
+{
+	return "Grade too low";
+}
