22d03fc4964ccdde063b0ab1fa91e6ff44291411
Tiago Martins <tiago86martins@gmail.com>
2024-08-01 15:14:58 +0100

    created Form class

---
+++ Module_05/ex01/includes/Form.hpp
@@ -0,0 +1,39 @@
+#include "Bureaucrat.hpp"
+#include <string>
+
+class Form
+{
+  private:
+	const std::string _name;
+	const int _gradeToSign;
+	const int _gradeToExecute;
+	bool _signed;
+
+  public:
+	Form ();
+	Form (const std::string name, const int gradeToSign,
+		  const int gradeToExecute);
+	Form (const Form &other);
+	Form &operator= (const Form &copy);
+	~Form ();
+
+	const std::string getName ();
+	int getGradeToSign ();
+	int getGradeToExecute ();
+	bool getSigned ();
+	void beSigned (Bureaucrat &bureaucrat);
+
+	class GradeTooHighException : public std::exception
+	{
+	  public:
+		const char *what () const throw ();
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+	  public:
+		const char *what () const throw ();
+	};
+};
+
+std::ostream &operator<< (std::ostream &out, const Form &form);
