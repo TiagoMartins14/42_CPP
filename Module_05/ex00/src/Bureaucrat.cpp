2aa90221ce962c1bbdd118585c20d298a7e0104f
TiagoMartins14 <92262694+TiagoMartins14@users.noreply.github.com>
2024-08-05 09:44:18 +0100

    clean up

--- Module_05/ex00/src/Bureaucrat.cpp
+++ Module_05/ex00/src/Bureaucrat.cpp
@@ -1,74 +1,84 @@
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat() : _name("no name"), _grade(150) {}
+Bureaucrat::Bureaucrat () : _name ("no name"), _grade (150) {}
 
-Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name)
+Bureaucrat::Bureaucrat (std::string name, int grade) : _name (name)
 {
 	if (grade < MAX_GRADE)
-	{
-		throw GradeTooHighException();
-	}
+		{
+			throw GradeTooHighException ();
+		}
 	else if (grade > MIN_GRADE)
-	{
-		throw GradeTooLowException();
-	}
+		{
+			throw GradeTooLowException ();
+		}
 	_grade = grade;
 }
 
-Bureaucrat::Bureaucrat(const Bureaucrat &other)
-{
-	*this = other;
-}
+Bureaucrat::Bureaucrat (const Bureaucrat &other) { *this = other; }
 
-Bureaucrat::~Bureaucrat() {}
+Bureaucrat::~Bureaucrat () {}
 
-Bureaucrat &Bureaucrat::operator=(const Bureaucrat &copy)
+Bureaucrat &
+Bureaucrat::operator= (const Bureaucrat &copy)
 {
 	if (this != &copy)
-	{
-		this->_name = copy.name;
-		this->_grade = copy.grade;
-	}
+		{
+			this->_name = copy._name;
+			this->_grade = copy._grade;
+		}
 	return *this;
 }
 
-std::string Bureaucrat::getName()
+std::string
+Bureaucrat::getName ()
 {
 	return _name;
 }
 
-int	Bureaucrat::getGrade()
+int
+Bureaucrat::getGrade ()
 {
 	return _grade;
 }
 
-void Bureaucrat::incrementGrade()
+void
+Bureaucrat::incrementGrade ()
 {
-	if (grade - 1 < MAX_GRADE)
-	{
-		throw GradeTooHighException();
-	}
-	grade--;
+	if (_grade - 1 < MAX_GRADE)
+		{
+			throw GradeTooHighException ();
+		}
+	_grade--;
 }
 
-void Bureaucrat::decrementGrade()
+void
+Bureaucrat::decrementGrade ()
 {
-	if (grade + 1 > MIN_GRADE)
-	{
-		throw GradeTooLowException();
-	}
-	grade++;
+	if (_grade + 1 > MIN_GRADE)
+		{
+			throw GradeTooLowException ();
+		}
+	_grade++;
 }
 
-const char* Bureaucrat::GradeTooHighExceptio::what() const throw()
+const char *
+Bureaucrat::GradeTooHighException::what () const throw ()
+{
 	return "Grade too high";
+}
 
-const char* Bureaucrat::GradeTooLowExceptio::what() const throw()
-	return "Grade too low";
-
-std::ostream &operator<<(std::ostream &out, Bureaucrat &bureaucrat)
+const char *
+Bureaucrat::GradeTooLowException::what () const throw ()
 {
-	out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << "." << std:: endl;
+	return "Grade too low";
+}
+
+std::ostream &
+operator<< (std::ostream &out, Bureaucrat &bureaucrat)
+{
+	out << bureaucrat.getName () << ", bureaucrat grade "
+		<< bureaucrat.getGrade () << "." << std::endl;
 
 	return out;
 }
--- Module_05/ex00/src/main.cpp
+++ Module_05/ex00/src/main.cpp
@@ -1,44 +1,45 @@
 #include "Bureaucrat.hpp"
 
-int main()
+int
+main ()
 {
 	try
-	{
-		Bureaucrat adam("Adam", 0);
-	}
-	catch (const Bureaucrat::GradeTooHighException& e)
-	{
-		std::cerr << "Error: " << e.what() << std::endl;
-	}
-	catch (const Bureaucrat::GradeTooLowException& e)
-	{
-		std::cerr << "Error: " << e.what() << std::endl;
-	}
+		{
+			Bureaucrat adam ("Adam", 0);
+		}
+	catch (const Bureaucrat::GradeTooHighException &e)
+		{
+			std::cerr << "Error: " << e.what () << std::endl;
+		}
+	catch (const Bureaucrat::GradeTooLowException &e)
+		{
+			std::cerr << "Error: " << e.what () << std::endl;
+		}
 
-	Bureaucrat *brian = nullptr;
+	Bureaucrat *brian = NULL;
 
 	try
-	{
-		brian = new Bureaucrat("Brian", 1);
-	}
-	catch (const Bureaucrat::GradeTooHighException& e)
-	{
-		std::cerr << "Error: " << e.what() << std::endl;
-	}
-	catch (const Bureaucrat::GradeTooLowException& e)
-	{
-		std::cerr << "Error: " << e.what() << std::endl;
-	}
-	if (brian != nullptr)
-	{
-		try
 		{
-			brian.incrementGrade();
+			brian = new Bureaucrat ("Brian", 1);
 		}
-		catch (const Bureaucrat::GradeTooHighException& e)
+	catch (const Bureaucrat::GradeTooHighException &e)
 		{
-			std::cerr << "Error: " << e.what() << std::endl;
+			std::cerr << "Error: " << e.what () << std::endl;
 		}
-	}
+	catch (const Bureaucrat::GradeTooLowException &e)
+		{
+			std::cerr << "Error: " << e.what () << std::endl;
+		}
+	if (brian != NULL)
+		{
+			try
+				{
+					brian->incrementGrade ();
+				}
+			catch (const Bureaucrat::GradeTooHighException &e)
+				{
+					std::cerr << "Error: " << e.what () << std::endl;
+				}
+		}
 	delete brian;
 }
