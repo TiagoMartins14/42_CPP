558757568a23df1821101d665bbaa53e48e1d6f5
Tiago Martins <tiago86martins@gmail.com>
2024-08-12 13:11:54 +0100

    deleted obsolete function

--- Module_05/ex03/includes/PresidentialPardonForm.hpp
+++ Module_05/ex03/includes/PresidentialPardonForm.hpp
@@ -1,22 +1,19 @@
 #pragma once
 #include "AForm.hpp"
 
-class PresidentialPardonForm : public AForm
-{
-  private:
+class PresidentialPardonForm : public AForm {
+   private:
 	std::string _target;
 
-  public:
-	PresidentialPardonForm (std::string target);
-	PresidentialPardonForm (const PresidentialPardonForm &other);
-	PresidentialPardonForm &operator= (const PresidentialPardonForm &copy);
-	~PresidentialPardonForm ();
+   public:
+	PresidentialPardonForm(std::string target);
+	PresidentialPardonForm(const PresidentialPardonForm &other);
+	PresidentialPardonForm &operator=(const PresidentialPardonForm &copy);
+	~PresidentialPardonForm();
 
-	void executePardon () const;
+	const std::string &getTarget() const;
 
-	const std::string &getTarget () const;
-
-	void executeAction () const;
+	void executeAction() const;
 
 	static int const signGrade = 25;
 	static int const executeGrade = 5;
