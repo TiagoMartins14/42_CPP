810cb535db71e409ab3036443689d69be7e7ed73
Tiago Martins <tiago86martins@gmail.com>
2024-08-12 13:50:43 +0100

    created Intern class

--- Module_05/ex03/includes/Intern.hpp
+++ Module_05/ex03/includes/Intern.hpp
@@ -1,21 +1,26 @@
 #pragma once
-#include "AForm.hpp"
-#include "PardonRequestForm.hpp"
-#include "RobotomyRequestForm.hpp"
-#include "ShrubberyCreationForm.hpp"
 #include <string>
 
-class Intern
-{
-  public:
+#include "AForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+
+class Intern {
+   private:
+	AForm *createShrubberyCreationForm(const std::string &target) const;
+	AForm *createRobotomyRequestForm(const std::string &target) const;
+	AForm *createPresidentialPardonForm(const std::string &target) const;
+
+   public:
 	// Constructors and destructor
-	Intern ();
-	Intern (const Intern &other);
-	~Intern ();
+	Intern();
+	Intern(const Intern &other);
+	~Intern();
 
 	// Overload
-	Intern &operator= (const Intern &copy);
+	Intern &operator=(const Intern &copy);
 
 	// Member function
-	AForm *makeForm (std::string &form, std::string &target);
+	AForm *makeForm(std::string &form, std::string &target) const;
 };
--- Module_05/ex03/src/Intern.cpp
+++ Module_05/ex03/src/Intern.cpp
@@ -1,15 +1,31 @@
 #include "Intern.hpp"
 
-Intern::Intern (const Intern &other) { *this = other; }
+Intern::Intern(const Intern &other) { *this = other; }
 
-Intern &
-Intern::operator= (const Intern &copy)
-{
+Intern &Intern::operator=(const Intern &copy) {
 	(void)copy;
 	return *this;
 }
 
-AForm *
-Intern::makeForm (std::string &form, std::string &target)
-{
+AForm *Intern::makeForm(std::string &form, std::string &target) const {
+	int formIndex = 0;
+	const std::string formList[3] = {"shrubbery creation", "robotmy request",
+									 "presidential pardon"};
+
+	while (formIndex < 3 && formList[formIndex] != form) formIndex++;
+
+	if (formIndex > 3) {
+		std::cout << "The intern does't know how to create such form! - "
+				  << form << std::endl;
+		return NULL;
+	}
+
+	AForm *(Intern::*createFormList[3])(const std::string &target)
+		const = {&Intern::createShrubberyCreationForm,
+				 &Intern::createRobotomyRequestForm,
+				 &Intern::createPresidentialPardonForm};
+
+	std::cout << "The Intern successfully created a " << form << std::endl;
+
+	return (this->*createFormList[formIndex])(target);
 }
