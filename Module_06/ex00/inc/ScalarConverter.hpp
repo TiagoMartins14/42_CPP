52eff22a4c4cb8448fe93713e69564031f06cf67
Tiago Martins <tiago86martins@gmail.com>
2024-08-20 19:04:54 +0100

    first commit

---
+++ Module_06/ex00/inc/ScalarConverter.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+enum e_inputType {
+	CHAR,
+	INT,
+	DOUBLE,
+	FLOAT,
+};
+
+class ScalarConverter {
+   public:
+	ScalarConverter(std::string input);
+	ScalarConverter(const ScalarConverter &other);
+	ScalarConverter &operator=(const ScalarConverter &copy);
+	~ScalarConverter();
+
+	static std::string invalidInputError();
+	static void convert(const std::string input);
+	static void printChar(std::string input);
+	static void printInt(std::string input);
+	static void printDouble(std::string input);
+	static void printFloat(std::string input);
+};
---
+++ Module_06/ex00/src/ScalarConverter.cpp
@@ -0,0 +1,52 @@
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter(std::string input) : _input(input) {}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &other)
+	: _input(other._input) {
+	*this = other;
+}
+
+ScalarConverter &ScalarConverter::operator=(const ScalarConverter &copy) {
+	return *this;
+}
+
+ScalarConverter::~ScalarConverter() {}
+
+std::string ScalarConverter::getInput() { return _input; }
+
+std::string invalidInputError() {
+	std::cerr
+		<< "Invalid input. Valid input: <char> or <int> or <double> or <float>"
+		<< std::endl;
+	exit(2);
+}
+
+void ScalarConverter::convert(const std::string input) {
+	int inputType;
+	int dotCounter = 0;
+
+	if (input.length() == 0)
+		invalidInputError();
+	else if (input.length() == 1) {
+		if (input[0] >= 48 && input[0] <= 57)
+			inputType = INT;
+		else if ((input[0] >= 65 && input[0] <= 90) ||
+				 (input[0] >= 97 && input[0] <= 122))
+			inputType = CHAR;
+	} else if (input.length() > 1) {
+		for (size_t i = 0; i < input.length(); i++) {
+			if (i == 0 && input[i] == '-') continue;
+			if (i == input.length() - 1 && input[i] == 'f' && dotCounter == 1) {
+				inputType = FLOAT;
+				break;
+			}
+			if (input[i] == '.') {
+				dotCounter++;
+			}
+			if (dotCounter > 1 || input[i] < 48 || input[i] > 57)
+				invalidInputError();
+		}
+		inputType = INT;
+	}
+}
