1370e8f045e3cefef34935075d6f15f0cdbbde2a
Tiago Martins <tiago86martins@gmail.com>
2024-08-25 19:11:33 +0100

    deleted print functions

--- Module_06/ex00/inc/ScalarConverter.hpp
+++ Module_06/ex00/inc/ScalarConverter.hpp
@@ -22,8 +22,4 @@ class ScalarConverter {
 
 	static std::string invalidInputError();
 	static void convert(const std::string input);
-	static void printChar(std::string input);
-	static void printInt(std::string input);
-	static void printDouble(std::string input);
-	static void printFloat(std::string input);
 };
