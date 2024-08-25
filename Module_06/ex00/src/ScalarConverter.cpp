40db5daa4c7889befce0d8a93e133770816395a3
Tiago Martins <tiago86martins@gmail.com>
2024-08-25 10:39:56 +0100

    added intChecker()

--- Module_06/ex00/src/ScalarConverter.cpp
+++ Module_06/ex00/src/ScalarConverter.cpp
@@ -1,9 +1,11 @@
 #include "ScalarConverter.hpp"
 
+#include <climits>
+#include <stdexcept>
+
 ScalarConverter::ScalarConverter(std::string input) : _input(input) {}
 
-ScalarConverter::ScalarConverter(const ScalarConverter &other)
-	: _input(other._input) {
+ScalarConverter::ScalarConverter(const ScalarConverter &other) {
 	*this = other;
 }
 
@@ -13,13 +15,21 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter &copy) {
 
 ScalarConverter::~ScalarConverter() {}
 
-std::string ScalarConverter::getInput() { return _input; }
-
 std::string invalidInputError() {
 	std::cerr
 		<< "Invalid input. Valid input: <char> or <int> or <double> or <float>"
 		<< std::endl;
 	exit(2);
+}
+
+bool intChecker(const std::string input) {
+	long num;
+
+	std::stringstream ss(input);
+	ss >> num;
+
+	if (ss.fail() || num < INT_MIN || num > INT_MAX) return false;
+	return true;
 }
 
 e_inputType inputTypeChecker(const std::string input) {
@@ -54,40 +64,11 @@ e_inputType inputTypeChecker(const std::string input) {
 			}
 		}
 	}
+	return INVALID;
 }
 
 void ScalarConverter::convert(const std::string input) {
-	int inputType;
-	int dotCounter = 0;
-
-	if (input.length() == 0)
-		invalidInputError();
-	else if (input.length() == 1) {
-		if (input[0] >= 48 && input[0] <= 57)
-			inputType = INT;
-		else if ((input[0] >= 65 && input[0] <= 90) ||
-				 (input[0] >= 97 && input[0] <= 122))
-			inputType = CHAR;
-	} else if (input.length() > 1) {
-		for (size_t i = 0; i < input.length(); i++) {
-			if (i == 0 && input[i] == '-') continue;
-			if (input[i] == '.') {
-				dotCounter++;
-			}
-			if (dotCounter > 1) invalidInputError();
-			if (input[i] < 48 || input[i] > 57 ||
-				(input[i] == 'f' && i != input.length() - 1))
-				invalidInputError();
-			if (i == input.length() - 1) {
-				if (dotCounter == 0)
-					inputType = INT;
-				else if (input[i] == 'f')
-					inputType = FLOAT;
-				else
-					inputType = DOUBLE;
-			}
-		}
-	}
+	int inputType = inputTypeChecker(input);
 	// Now print the right conversions
 	switch (inputType) {
 		default:
