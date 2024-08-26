91c3952294b8cce3b651d2da085775dbff16cd17
tiaferna <tiaferna@student.42.fr>
2024-08-26 16:23:29 +0100

    updated error msg

--- Module_06/ex00/src/main.cpp
+++ Module_06/ex00/src/main.cpp
@@ -2,9 +2,10 @@
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    std::cerr
-        << "Invalid input. Valid input: <char> or <int> or <double> or <float>"
-        << std::endl;
+    std::cerr << "Invalid input. Valid input: ONE <char> or <int> or <double> "
+                 "or <float>"
+              << std::endl;
+    return 1;
   }
   ScalarConverter::convert(argv[1]);
   return 0;
