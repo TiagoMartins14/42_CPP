77f055593ce9755b3f21b8d1e6c39a9b5d26b961
tiaferna <tiaferna@student.42.fr>
2024-08-26 16:20:39 +0100

    added input checker and error message

--- Module_06/ex00/src/main.cpp
+++ Module_06/ex00/src/main.cpp
@@ -1,8 +1,11 @@
 #include "ScalarConverter.hpp"
 
 int main(int argc, char **argv) {
-	if (argc == 2) {
-		ScalarConverter::convert(argv[1]);
-	}
-	return 0;
+  if (argc != 2) {
+    std::cerr
+        << "Invalid input. Valid input: <char> or <int> or <double> or <float>"
+        << std::endl;
+  }
+  ScalarConverter::convert(argv[1]);
+  return 0;
 }
