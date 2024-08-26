35a4fd67a37a828407e863303b8c8b4c5cfa7a2b
tiaferna <tiaferna@student.42.fr>
2024-08-26 15:20:04 +0100

    corrected double and float display for all cases expet the special ones

--- Module_06/ex00/inc/ScalarConverter.hpp
+++ Module_06/ex00/inc/ScalarConverter.hpp
@@ -8,12 +8,12 @@
 enum e_inputType { CHAR, INT, DOUBLE, FLOAT, INVALID };
 
 class ScalarConverter {
-   public:
-	ScalarConverter();
-	ScalarConverter(const ScalarConverter &other);
-	ScalarConverter &operator=(const ScalarConverter &copy);
-	~ScalarConverter();
+public:
+  ScalarConverter();
+  ScalarConverter(const ScalarConverter &other);
+  ScalarConverter &operator=(const ScalarConverter &copy);
+  ~ScalarConverter();
 
-	static std::string invalidInputError();
-	static void convert(const std::string &input);
+  static std::string invalidInputError();
+  static void convert(const std::string &input);
 };
