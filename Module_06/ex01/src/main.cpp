0166c8e0d0c346b4753355f899af43c6abd0d2f3
tiaferna <tiaferna@student.42.fr>
2024-08-27 09:52:35 +0100

    changed wording

--- Module_06/ex01/src/main.cpp
+++ Module_06/ex01/src/main.cpp
@@ -5,7 +5,7 @@ int main() {
   Data mazda("Mazda", "RX8", 2002, 3000, false);
 
   uintptr_t mazdaUIntPtr = Serializer::serialize(&mazda);
-  std::cout << "mazda pointer: " << &mazda << std::endl;
+  std::cout << "mazda address: " << &mazda << std::endl;
   std::cout << "serialize: " << mazdaUIntPtr << std::endl;
   std::cout << "deserialize " << Serializer::deserialize(mazdaUIntPtr)
             << std::endl;
