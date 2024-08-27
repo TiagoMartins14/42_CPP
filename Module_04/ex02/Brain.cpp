418ca19e21de81f508b16612467141a4df19760a
tiaferna <tiaferna@student.42.fr>
2024-08-27 11:00:47 +0100

    updated to canonical norm form

--- Module_04/ex02/Brain.cpp
+++ Module_04/ex02/Brain.cpp
@@ -12,13 +12,42 @@
 
 #include "Brain.hpp"
 
+#define ARRAY_SIZE 100
+
 Brain::Brain()
 {
 	std::cout << "Brain constructor called" << std::endl;
+
+	this->_ideas = new std::string[ARRAY_SIZE];
+}
+
+Brain::Brain(const Brain &other) 
+{
+	std::cout << "Copy Brain constructor called" << std::endl;
+
+	this->_ideas = new std::string[ARRAY_SIZE];
+	for (int i = 0; i < ARRAY_SIZE; i++)
+		this->_ideas[i] = other._ideas[i];
+}
+
+Brain &Brain::operator=(const Brain &copy)
+{
+	std::cout << "Brain = assignment operator called" << std::endl;
+
+	if (this != &copy)
+	{
+		delete[] this->_ideas;
+		this->_ideas = new std::string[ARRAY_SIZE];
+		for (int i = 0; i < ARRAY_SIZE; i++)
+			this->_ideas[i] = copy._ideas[i];
+	}
+	return *this;
 }
 
 Brain::~Brain()
 {
 	std::cout << "Brain destructor called" << std::endl;
+
+	delete[] this->_ideas;
 }
 
