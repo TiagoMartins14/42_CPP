c1c775a30fc68518a9750059cf9071adda5a45f0
Tiago Martins <tiago86martins@gmail.com>
2024-08-29 22:07:28 +0100

    templates defined

--- Module_07/ex00/whatever.hpp
+++ Module_07/ex00/whatever.hpp
@@ -1,10 +1,19 @@
 #pragma once
 
 template <typename T>
-void swap(T &val1, T &val2);
+void swap(T &val1, T &val2) {
+	T temp = val1;
+
+	val1 = val2;
+	val2 = temp;
+}
 
 template <typename T>
-T min(T val1, T val2);
+T min(T val1, T val2) {
+	return (val1 < val2) ? val1 : val2;
+}
 
 template <typename T>
-T max(T val1, T val2);
+T max(T val1, T val2) {
+	return (val1 > val2) ? val1 : val2;
+}
