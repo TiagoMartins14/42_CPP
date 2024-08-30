997fb7d6f6b675f95f6ade957a217816fe241bc1
tiaferna <tiaferna@student.42.fr>
2024-08-30 18:09:46 +0100

    created test with default and non default Array

--- Module_07/ex02/main.cpp
+++ Module_07/ex02/main.cpp
@@ -1,3 +1,15 @@
-#include "Array.hpp"
+#include "Array.tpp"
+#include <iostream>
 
-int main() { return 0; }
+int main() {
+
+  Array<int> *intArray = new Array<int>;
+  Array<float> *floatArray = new Array<float>(14);
+
+  std::cout << intArray->size() << std::endl;
+  std::cout << floatArray->size() << std::endl;
+
+  delete intArray;
+  delete floatArray;
+  return 0;
+}
