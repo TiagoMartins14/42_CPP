d98acd4aba6e8a8dcec133a8697a35471a80d167
tiaferna <tiaferna@student.42.fr>
2024-08-30 15:05:35 +0100

    fixed char conversion and float display bug

--- Module_06/ex00/src/ScalarConverter.cpp
+++ Module_06/ex00/src/ScalarConverter.cpp
@@ -47,6 +47,8 @@ static int decimalCount(const std::string &input) {
   size_t dotPos = input.find('.');
   if (dotPos == std::string::npos)
     return 1;
+  if (inputTypeChecker(input) == FLOAT)
+    return input.length() - dotPos - 2;
   return input.length() - dotPos - 1;
 }
 
@@ -61,7 +63,7 @@ static void printChar(const std::string &input) {
     int num;
 
     ss >> num;
-    if (num < 33 || num > 126)
+    if (num < 32 || num > 126)
       std::cout << "impossible";
     else
       std::cout << "'" << static_cast<char>(num) << "'";
@@ -71,18 +73,24 @@ static void printChar(const std::string &input) {
 }
 
 static void printInt(const std::string &input) {
-  std::stringstream ss(input);
-  int num;
 
-  ss >> num;
   std::cout << "int: ";
-  if (!ss.fail()) {
-    std::cout << static_cast<int>(num);
-  } else
-    std::cout << "impossible";
-  std::cout << std::endl;
-}
 
+  if (input.length() == 1 &&
+      ((input[0] > 31 && input[0] < 48) || (input[0] > 57 && input[0] < 127)))
+    std::cout << static_cast<int>(input[0]) << std::endl;
+  else {
+    std::stringstream ss(input);
+    int num;
+    ss >> num;
+
+    if (!ss.fail()) {
+      std::cout << static_cast<int>(num);
+    } else
+      std::cout << "impossible";
+    std::cout << std::endl;
+  }
+}
 static void printDouble(const std::string &input) {
   std::cout << "double: ";
   if (input == "+inf" || input == "+inff")
@@ -91,6 +99,9 @@ static void printDouble(const std::string &input) {
     std::cout << -std::numeric_limits<double>::infinity();
   else if (input == "nan" || input == "nanf")
     std::cout << std::numeric_limits<double>::quiet_NaN();
+  else if (input.length() == 1 && ((input[0] > 31 && input[0] < 48) ||
+                                   (input[0] > 57 && input[0] < 127)))
+    std::cout << static_cast<double>(input[0]) << ".0";
   else {
 
     std::stringstream ss(input);
@@ -98,10 +109,7 @@ static void printDouble(const std::string &input) {
 
     ss >> num;
     if (!ss.fail()) {
-      if (inputTypeChecker(input) == FLOAT)
-        std::cout.precision(decimalCount(input) - 1);
-      else
-        std::cout.precision(decimalCount(input));
+      std::cout.precision(decimalCount(input));
       std::cout << std::fixed << std::showpoint << static_cast<double>(num);
     } else
       std::cout << "impossible";
@@ -117,6 +125,9 @@ static void printFloat(const std::string &input) {
     std::cout << -std::numeric_limits<double>::infinity() << "f";
   else if (input == "nan" || input == "nanf")
     std::cout << std::numeric_limits<double>::quiet_NaN() << "f";
+  else if (input.length() == 1 && ((input[0] > 31 && input[0] < 48) ||
+                                   (input[0] > 57 && input[0] < 127)))
+    std::cout << static_cast<float>(input[0]) << ".0f";
   else {
     std::stringstream ss(input);
     float num;
