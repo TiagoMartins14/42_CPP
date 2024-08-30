374435c0d5b764a7cb46f15582223e310031bb95
tiaferna <tiaferna@student.42.fr>
2024-08-30 17:14:53 +0100

    added canonical functions

--- Module_07/ex02/Array.cpp
+++ Module_07/ex02/Array.cpp
@@ -11,3 +11,17 @@ template <typename T> Array<T>::Array(unsigned int n) {
   _size = n;
   _array = new T[_size];
 }
+
+template <typename T> Array<T>::Array(const Array &copy) {
+  if (this != &copy)
+    *this = copy;
+}
+
+template <typename T> Array<T> &Array<T>::operator=(const Array &copy) {
+  this->_size = copy._size;
+  this->_array = new T[this->_size];
+  for (int i = 0; i < this->_size; i++)
+    this->_array[i] = copy._array[i];
+
+  return *this;
+}
