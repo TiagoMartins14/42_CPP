10c53409c8d15daf492b172bf984e5f8cb2b80bf
Tiago Martins <tiago86martins@gmail.com>
2024-08-31 14:48:44 +0100

    copied subject main

--- Module_07/ex02/main.cpp
+++ Module_07/ex02/main.cpp
@@ -1,15 +1,43 @@
-#include "Array.tpp"
 #include <iostream>
 
-int main() {
+#include "Array.tpp"
 
-  Array<int> *intArray = new Array<int>;
-  Array<float> *floatArray = new Array<float>(14);
+#define MAX_VAL 750
+int main(int, char**) {
+	Array<int> numbers(MAX_VAL);
+	int* mirror = new int[MAX_VAL];
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++) {
+		const int value = rand();
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+	// SCOPE
+	{
+		Array<int> tmp = numbers;
+		Array<int> test(tmp);
+	}
 
-  std::cout << intArray->size() << std::endl;
-  std::cout << floatArray->size() << std::endl;
+	for (int i = 0; i < MAX_VAL; i++) {
+		if (mirror[i] != numbers[i]) {
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return 1;
+		}
+	}
+	try {
+		numbers[-2] = 0;
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << '\n';
+	}
+	try {
+		numbers[MAX_VAL] = 0;
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << '\n';
+	}
 
-  delete intArray;
-  delete floatArray;
-  return 0;
+	for (int i = 0; i < MAX_VAL; i++) {
+		numbers[i] = rand();
+	}
+	delete[] mirror;  //
+	return 0;
 }
