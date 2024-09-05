1cb29f9cc18dc246f234a3fe4cb7f1c9c7372a65
tiaferna <tiaferna@student.42.fr>
2024-09-05 11:11:16 +0100

    tweaked << operator overload

--- Module_05/ex01/src/Bureaucrat.cpp
+++ Module_05/ex01/src/Bureaucrat.cpp
@@ -88,10 +88,10 @@ Bureaucrat::GradeTooLowException::what () const throw ()
 }
 
 std::ostream &
-operator<< (std::ostream &out, Bureaucrat &bureaucrat)
+operator<< (std::ostream &out, const Bureaucrat &bureaucrat)
 {
 	out << bureaucrat.getName () << ", bureaucrat grade "
-		<< bureaucrat.getGrade () << "." << std::endl;
+		<< bureaucrat.getGrade () << ".";
 
 	return out;
 }
