a1aae3e9a58711b370a117c835e5d32406182129
tiaferna <tiaferna@student.42.fr>
2024-09-05 10:47:17 +0100

    added test to decrement grade as low as possible until it throws an exception

--- Module_05/ex00/src/main.cpp
+++ Module_05/ex00/src/main.cpp
@@ -41,5 +41,17 @@ main ()
 					std::cerr << "Error: " << e.what () << std::endl;
 				}
 		}
+	for (int i = 1; i <= MIN_GRADE; i++)
+	{
+		std::cout << *brian << " It will be decremented by one..." << std::endl; 
+		try
+			{
+				brian->decrementGrade ();
+			}
+		catch (const Bureaucrat::GradeTooLowException &e)
+			{
+				std::cerr << "Error: " << e.what () << std::endl;
+			}
+	}
 	delete brian;
 }
