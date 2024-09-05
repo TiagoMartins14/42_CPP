83295842416394958ac2cce9b8053918a32f75dc
tiaferna <tiaferna@student.42.fr>
2024-09-05 11:12:22 +0100

    updated the test to be more visual on what is happening behind the scenes

--- Module_05/ex01/src/main.cpp
+++ Module_05/ex01/src/main.cpp
@@ -6,7 +6,14 @@ main ()
 {
 	Bureaucrat *B1 = new Bureaucrat ("B1", 5);
 	Form *F1 = new Form ("F1", 4, 2);
+
+	std::cout << *B1 << std::endl;
+	std::cout << *F1 << std::endl;
+	std::cout << "Bureaucrat B1 is attempting to sign form F1..." << std::endl;
 	B1->signForm (*F1);
+	std::cout << "B1 is getting his grade incremented by one." << std::endl;
 	B1->incrementGrade ();
+	std::cout << *B1 << std::endl;
+	std::cout << "Bureaucrat B1 is attempting to sign form F1..." << std::endl;
 	B1->signForm (*F1);
 }
