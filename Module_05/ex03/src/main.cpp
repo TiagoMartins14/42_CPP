2e8366db6f83631f71a5f60da2e24d3bcb8e25c7
Tiago Martins <tiago86martins@gmail.com>
2024-09-06 15:38:34 +0100

    updated test for a bureaucrat to use a form created by an intern

--- Module_05/ex03/src/main.cpp
+++ Module_05/ex03/src/main.cpp
@@ -11,14 +11,21 @@ int main() {
 	AForm* robotForm;
 	AForm* presidForm;
 	AForm* fakeNewsForm;
+	Bureaucrat* bureaucrat;
 
 	shrubForm = someRandomIntern.makeForm("shrubbery creation", "Bender");
 	robotForm = someRandomIntern.makeForm("robotomy request", "Cleo");
 	presidForm = someRandomIntern.makeForm("presidential pardon", "Donald");
 	fakeNewsForm = someRandomIntern.makeForm("fake news", "Easton");
 
+	bureaucrat = new Bureaucrat("Andrew", 1);
+
+	bureaucrat->signForm(*robotForm);
+	robotForm->execute(*bureaucrat);
+
 	delete shrubForm;
 	delete robotForm;
 	delete presidForm;
 	delete fakeNewsForm;
+	delete bureaucrat;
 }
