18cf7a9ef4a7d2f45ab8bee5e2125c619b060cd6
Tiago Martins <tiago86martins@gmail.com>
2024-10-05 16:07:29 +0100

    updated main

--- Module_09/ex00/sources/main.cpp
+++ Module_09/ex00/sources/main.cpp
@@ -1,3 +1,9 @@
 #include "BitcoinExchange.hpp"
 
-int main() {}
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		std::cout << "Invalid input. Valid input: ./btc [file_name].txt"
+				  << std::endl;
+		return 1;
+	}
+}
