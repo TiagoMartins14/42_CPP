1d970381306f17f7f851acf349a8f324796ace01
tiaferna <tiaferna@student.42.fr>
2024-10-22 10:22:09 +0100

    added exceptions in case of creating a form with grades out of bounds

--- Module_05/ex01/src/Form.cpp
+++ Module_05/ex01/src/Form.cpp
@@ -5,6 +5,11 @@ Form::Form (const std::string name, const int gradeToSign,
 	: _name (name), _gradeToSign (gradeToSign),
 	  _gradeToExecute (gradeToExecute), _signed (false)
 {
+	if (gradeToSign <= MAX_GRADE || gradeToExecute <= MAX_GRADE)
+		throw GradeTooHighException();
+
+	if (gradeToSign >= MIN_GRADE || gradeToExecute >= MIN_GRADE)
+		throw GradeTooLowException();
 }
 
 Form::Form (const Form &other)
