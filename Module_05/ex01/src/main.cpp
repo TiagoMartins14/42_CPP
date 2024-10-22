e1915df864cc5489827c6e9fbf61ce173b1b29cb
tiaferna <tiaferna@student.42.fr>
2024-10-22 10:22:31 +0100

    updated main to catch Form exceptions

--- Module_05/ex01/src/main.cpp
+++ Module_05/ex01/src/main.cpp
@@ -4,9 +4,90 @@
 int
 main ()
 {
-	Bureaucrat *B1 = new Bureaucrat ("B1", 5);
-	Form *F1 = new Form ("F1", 4, 2);
+// Creating forms with grades out of bounds
+	try
+	{
+		Form F1 ("F1", 151, 10);
+	}
+	catch (const Form::GradeTooHighException &e)
+	{
+		std::cerr << "Error: " << e.what () << std::endl;
+	}
+	catch (const Form::GradeTooLowException &e)
+	{
+		std::cerr << "Error: " << e.what () << std::endl;
+	}
 
+	try
+	{
+		Form F1 ("F1", 10, 151);
+	}
+	catch (const Form::GradeTooHighException &e)
+	{
+		std::cerr << "Error: " << e.what () << std::endl;
+	}
+	catch (const Form::GradeTooLowException &e)
+	{
+		std::cerr << "Error: " << e.what () << std::endl;
+	}
+
+	try
+	{
+		Form F1 ("F1", 0, 10);
+	}
+	catch (const Form::GradeTooHighException &e)
+	{
+		std::cerr << "Error: " << e.what () << std::endl;
+	}
+	catch (const Form::GradeTooLowException &e)
+	{
+		std::cerr << "Error: " << e.what () << std::endl;
+	}
+
+	try
+	{
+		Form F1 ("F1", 10, 0);
+	}
+	catch (const Form::GradeTooHighException &e)
+	{
+		std::cerr << "Error: " << e.what () << std::endl;
+	}
+	catch (const Form::GradeTooLowException &e)
+	{
+		std::cerr << "Error: " << e.what () << std::endl;
+	}
+
+// Creating forms with valid grades
+
+	Form *F1 = NULL;
+	Bureaucrat *B1 = NULL;
+
+	try
+	{
+		F1 = new Form ("F1", 4, 2);
+	}
+	catch (const Form::GradeTooHighException &e)
+	{
+		std::cerr << "Error: " << e.what () << std::endl;
+	}
+	catch (const Form::GradeTooLowException &e)
+	{
+		std::cerr << "Error: " << e.what () << std::endl;
+	}
+
+	try
+	{
+		B1 = new Bureaucrat ("B1", 5);
+	}
+	catch (const Bureaucrat::GradeTooHighException &e)
+	{
+		std::cerr << "Error: " << e.what () << std::endl;
+	}
+	catch (const Bureaucrat::GradeTooLowException &e)
+	{
+		std::cerr << "Error: " << e.what () << std::endl;
+	}
+	
 	std::cout << *B1 << std::endl;
 	std::cout << *F1 << std::endl;
 	std::cout << "Bureaucrat B1 is attempting to sign form F1..." << std::endl;
