dd1e326b95d0ebf6a4c9ec38fe50602d5b1da0fd
tiaferna <tiaferna@student.42.fr>
2024-10-24 16:25:57 +0100

    correctly handles overflows of all types

--- Module_06/ex00/inc/ScalarConverter.hpp
+++ Module_06/ex00/inc/ScalarConverter.hpp
@@ -10,26 +10,26 @@
 enum e_inputType { CHAR, INT, DOUBLE, FLOAT, PSEUDO_LITERAL, INVALID };
 
 class ScalarConverter {
-   public:
-	ScalarConverter();
-	ScalarConverter(const ScalarConverter &other);
-	ScalarConverter &operator=(const ScalarConverter &copy);
-	~ScalarConverter();
+public:
+  ScalarConverter();
+  ScalarConverter(const ScalarConverter &other);
+  ScalarConverter &operator=(const ScalarConverter &copy);
+  ~ScalarConverter();
 
-	static void convert(std::string &input);
+  static void convert(std::string &input);
 
-   private:
-	static bool isChar(std::string &input);
-	static bool isInt(std::string &input);
-	static bool isFloat(std::string &input);
-	static bool isDouble(std::string &input);
-	static bool isPseudoLiteral(std::string &input);
-	static e_inputType inputTypeChecker(std::string &input);
-	static void printImpossibleInput();
-	static void printPseudoLiterals(std::string &input);
-	static void convertToChar(char c);
-	static void convertToInt(int integer);
-	static void convertToFloat(float fp);
-	static void convertToDouble(double dp);
-	static void converterFunction(long double num);
+private:
+  static bool isChar(std::string &input);
+  static bool isInt(std::string &input);
+  static bool isFloat(std::string &input);
+  static bool isDouble(std::string &input);
+  static bool isPseudoLiteral(std::string &input);
+  static e_inputType inputTypeChecker(std::string &input);
+  static void printImpossibleInput();
+  static void printPseudoLiterals(std::string &input);
+  static void convertToChar(std::string &input);
+  static void convertToInt(std::string &input);
+  static void convertToFloat(std::string &input);
+  static void convertToDouble(std::string &input);
+  static void converterFunction(std::string &input);
 };
--- Module_06/ex00/src/ScalarConverter.cpp
+++ Module_06/ex00/src/ScalarConverter.cpp
@@ -2,165 +2,232 @@
 #include <cctype>
 #include <cstring>
 #include <iomanip>
+#include <limits>
+#include <stdexcept>
 
 #include "ScalarConverter.hpp"
 
 bool ScalarConverter::isChar(std::string &input) {
-	return (input.length() == 1 && isprint(input[0]) && !isdigit(input[0]));
+  return (input.length() == 1 && isprint(input[0]) && !isdigit(input[0]));
 }
 
 bool ScalarConverter::isInt(std::string &input) {
-	std::string allowedInput = "0123456789";
-	std::string str = input;
+  std::string allowedInput = "0123456789";
+  std::string str = input;
 
-	if (input.size() > 0 && (input[0] == '+' || input[0] == '-'))
-		str = str.substr(1);
+  if (input.size() > 0 && (input[0] == '+' || input[0] == '-'))
+    str = str.substr(1);
 
-	return (str.find_first_not_of(allowedInput) == std::string::npos &&
-			str.find('.') == std::string::npos);
+  return (str.find_first_not_of(allowedInput) == std::string::npos &&
+          str.find('.') == std::string::npos);
 }
 
 bool ScalarConverter::isFloat(std::string &input) {
-	std::string allowedInput = "0123456789.f";
-	std::string str = input;
+  std::string allowedInput = "0123456789.f";
+  std::string str = input;
 
-	if (input.size() > 0 && (input[0] == '+' || input[0] == '-'))
-		str = str.substr(1);
+  if (input.size() > 0 && (input[0] == '+' || input[0] == '-'))
+    str = str.substr(1);
 
-	if (str.length() < 4) return false;
+  if (str.length() < 4)
+    return false;
 
-	if (str.find('.') == std::string::npos ||
-		str.find('f') == std::string::npos)
-		return false;
+  if (str.find('.') == std::string::npos || str.find('f') == std::string::npos)
+    return false;
 
-	if (str.find('f') != str.length() - 1) return false;
+  if (str.find('f') != str.length() - 1)
+    return false;
 
-	if (str.find('.') == str.length() - 2) return false;
+  if (str.find('.') == str.length() - 2)
+    return false;
 
-	if (str.find_first_of('.') != str.find_last_of('.') ||
-		str.find_first_of('f') != str.find_last_of('f'))
-		return false;
+  if (str.find_first_of('.') != str.find_last_of('.') ||
+      str.find_first_of('f') != str.find_last_of('f'))
+    return false;
 
-	return true;
+  return true;
 }
 
 bool ScalarConverter::isDouble(std::string &input) {
-	std::string allowedInput = "0123456789.";
-	std::string str = input;
+  std::string allowedInput = "0123456789.";
+  std::string str = input;
 
-	if (input.size() > 0 && (input[0] == '+' || input[0] == '-'))
-		str = str.substr(1);
+  if (input.size() > 0 && (input[0] == '+' || input[0] == '-'))
+    str = str.substr(1);
 
-	if (str.length() < 3) return false;
+  if (str.length() < 3)
+    return false;
 
-	if (str.find('.') == std::string::npos) return false;
+  if (str.find('.') == std::string::npos)
+    return false;
 
-	if (str.find_first_of('.') != str.find_last_of('.')) return false;
+  if (str.find_first_of('.') != str.find_last_of('.'))
+    return false;
 
-	if (str.find('.') == str.length() - 1) return false;
+  if (str.find('.') == str.length() - 1)
+    return false;
 
-	return true;
+  return true;
 }
 
 bool ScalarConverter::isPseudoLiteral(std::string &input) {
-	if (strcmp(input.c_str(), "nan") == 0 ||
-		strcmp(input.c_str(), "nanf") == 0 ||
-		strcmp(input.c_str(), "-inf") == 0 ||
-		strcmp(input.c_str(), "+inf") == 0 ||
-		strcmp(input.c_str(), "-inff") == 0 ||
-		strcmp(input.c_str(), "+inff") == 0)
-		return true;
-	return false;
+  if (strcmp(input.c_str(), "nan") == 0 || strcmp(input.c_str(), "nanf") == 0 ||
+      strcmp(input.c_str(), "-inf") == 0 ||
+      strcmp(input.c_str(), "+inf") == 0 ||
+      strcmp(input.c_str(), "-inff") == 0 ||
+      strcmp(input.c_str(), "+inff") == 0)
+    return true;
+  return false;
 }
 
 e_inputType ScalarConverter::inputTypeChecker(std::string &input) {
-	if (isChar(input)) return CHAR;
-	if (isInt(input)) return INT;
-	if (isFloat(input)) return FLOAT;
-	if (isDouble(input)) return DOUBLE;
-	if (isPseudoLiteral(input)) return PSEUDO_LITERAL;
-	return INVALID;
+  if (isChar(input))
+    return CHAR;
+  if (isInt(input))
+    return INT;
+  if (isFloat(input))
+    return FLOAT;
+  if (isDouble(input))
+    return DOUBLE;
+  if (isPseudoLiteral(input))
+    return PSEUDO_LITERAL;
+  return INVALID;
 }
 
 void ScalarConverter::printImpossibleInput() {
-	std::cout << "char: impossible" << std::endl;
-	std::cout << "int: impossible" << std::endl;
-	std::cout << "float: impossible" << std::endl;
-	std::cout << "double: impossible" << std::endl;
+  std::cout << "char: impossible" << std::endl;
+  std::cout << "int: impossible" << std::endl;
+  std::cout << "float: impossible" << std::endl;
+  std::cout << "double: impossible" << std::endl;
 }
 
 void ScalarConverter::printPseudoLiterals(std::string &input) {
-	std::cout << "char: impossible" << std::endl;
-	std::cout << "int: impossible" << std::endl;
+  std::cout << "char: impossible" << std::endl;
+  std::cout << "int: impossible" << std::endl;
 
-	if (strcmp(input.c_str(), "nan") == 0 ||
-		strcmp(input.c_str(), "nanf") == 0) {
-		std::cout << "float: nanf" << std::endl;
-		std::cout << "double: nan" << std::endl;
-	}
+  if (strcmp(input.c_str(), "nan") == 0 || strcmp(input.c_str(), "nanf") == 0) {
+    std::cout << "float: nanf" << std::endl;
+    std::cout << "double: nan" << std::endl;
+  }
 
-	if (strcmp(input.c_str(), "-inf") == 0 ||
-		strcmp(input.c_str(), "-inff") == 0) {
-		std::cout << "float: -inff" << std::endl;
-		std::cout << "double: -inf" << std::endl;
-	}
+  if (strcmp(input.c_str(), "-inf") == 0 ||
+      strcmp(input.c_str(), "-inff") == 0) {
+    std::cout << "float: -inff" << std::endl;
+    std::cout << "double: -inf" << std::endl;
+  }
 
-	if (strcmp(input.c_str(), "+inf") == 0 ||
-		strcmp(input.c_str(), "+inff") == 0) {
-		std::cout << "float: +inff" << std::endl;
-		std::cout << "double: +inf" << std::endl;
-	}
+  if (strcmp(input.c_str(), "+inf") == 0 ||
+      strcmp(input.c_str(), "+inff") == 0) {
+    std::cout << "float: +inff" << std::endl;
+    std::cout << "double: +inf" << std::endl;
+  }
 }
 
-void ScalarConverter::convertToChar(char c) {
-	std::cout << "char: ";
+bool overflowChecker(long double num, e_inputType type) {
+  switch (type) {
+  case INVALID:
 
-	if (!isprint(c))
-		std::cout << "Non displayable" << std::endl;
-	else
-		std::cout << "'" << c << "'" << std::endl;
+  case PSEUDO_LITERAL:
+
+  case CHAR:
+    return num < std::numeric_limits<char>::min() ||
+           num > std::numeric_limits<char>::max();
+
+  case INT:
+    return num < std::numeric_limits<int>::min() ||
+           num > std::numeric_limits<int>::max();
+
+  case FLOAT:
+    return num < -std::numeric_limits<float>::max() ||
+           num > std::numeric_limits<float>::max();
+
+  case DOUBLE:
+    return num < -std::numeric_limits<double>::max() ||
+           num > std::numeric_limits<double>::max();
+  }
 }
 
-void ScalarConverter::convertToInt(int integer) {
-	std::cout << "int :" << integer << std::endl;
+void ScalarConverter::convertToChar(std::string &input) {
+
+  char c = input[0];
+  long double num = input[0];
+
+  std::cout << "char: ";
+
+  if (!isprint(c))
+    std::cout << "Non displayable" << std::endl;
+  else if (overflowChecker(num, CHAR))
+    std::cout << "Overflowed" << std::endl;
+  else
+    std::cout << "'" << c << "'" << std::endl;
 }
 
-void ScalarConverter::convertToFloat(float fp) {
-	std::cout << "float: " << std::fixed << std::setprecision(1) << fp << "f"
-			  << std::endl;
+void ScalarConverter::convertToInt(std::string &input) {
+  long double num = std::atol(input.c_str());
+
+  std::cout << "int: ";
+
+  if (overflowChecker(num, INT))
+    std::cout << "Overflowed" << std::endl;
+  else
+    std::cout << num << std::endl;
 }
 
-void ScalarConverter::convertToDouble(double dp) {
-	std::cout << "double: " << std::fixed << std::setprecision(1) << dp
-			  << std::endl;
+void ScalarConverter::convertToFloat(std::string &input) {
+  char *endPtr;
+  long double num = std::strtod(input.c_str(), &endPtr);
+
+  std::cout << "float: ";
+  if (*endPtr != '\0') {
+    if (overflowChecker(num, FLOAT))
+      std::cout << "Overflowed" << std::endl;
+    else
+      std::cout << std::fixed << std::setprecision(1) << num << "f"
+                << std::endl;
+  } else
+    std::cout << "Overflowed" << std::endl;
 }
 
-void ScalarConverter::converterFunction(long double num) {
-	convertToChar(static_cast<char>(num));
-	convertToInt(static_cast<int>(num));
-	convertToFloat(static_cast<float>(num));
-	convertToDouble(static_cast<double>(num));
+void ScalarConverter::convertToDouble(std::string &input) {
+  char *endPtr;
+  long double num = std::strtold(input.c_str(), &endPtr);
+
+  std::cout << "double: ";
+  if (*endPtr != '\0') {
+    if (overflowChecker(num, DOUBLE))
+      std::cout << "Overflowed" << std::endl;
+    else
+      std::cout << std::fixed << std::setprecision(1) << num << std::endl;
+  } else
+    std::cout << "Overflowed" << std::endl;
+}
+
+void ScalarConverter::converterFunction(std::string &input) {
+  convertToChar(input);
+  convertToInt(input);
+  convertToFloat(input);
+  convertToDouble(input);
 }
 
 void ScalarConverter::convert(std::string &input) {
-	switch (inputTypeChecker(input)) {
-		case CHAR:
-			converterFunction(input[0]);
-			break;
-		case INT:
-			converterFunction(std::atoi(input.c_str()));
-			break;
-		case FLOAT:
-			converterFunction(std::strtof(input.c_str(), NULL));
-			break;
-		case DOUBLE:
-			converterFunction(std::strtold(input.c_str(), NULL));
-			break;
-		case PSEUDO_LITERAL:
-			printPseudoLiterals(input);
-			break;
-		case INVALID:
-			printImpossibleInput();
-			break;
-	}
+  switch (inputTypeChecker(input)) {
+  case CHAR:
+    converterFunction(input);
+    break;
+  case INT:
+    converterFunction(input);
+    break;
+  case FLOAT:
+    converterFunction(input);
+    break;
+  case DOUBLE:
+    converterFunction(input);
+    break;
+  case PSEUDO_LITERAL:
+    printPseudoLiterals(input);
+    break;
+  case INVALID:
+    printImpossibleInput();
+    break;
+  }
 }
