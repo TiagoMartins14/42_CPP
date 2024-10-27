3d41f7f91edadbfee2326d82be905294315b54c8
Tiago Martins <tiago86martins@gmail.com>
2024-10-27 11:57:28 +0000

    added tests to showcase the Span class

--- Module_08/ex01/sources/main.cpp
+++ Module_08/ex01/sources/main.cpp
@@ -1,3 +1,30 @@
 #include "Span.hpp"
+#include <iostream>
 
-int main() { return 0; }
+int main()
+{
+
+    Span sp = Span(5);
+
+    /* int spanArray[] = {2, 7, 14, 20, 40};
+
+    sp.setSequence(spanArray, sizeof(spanArray) / sizeof(int));
+    sp.fillSpan(); */
+
+
+    sp.addNumber(6);
+    sp.addNumber(3);
+    sp.addNumber(17);
+    sp.addNumber(9);
+    sp.addNumber(11);
+
+    try {
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
+    }
+    catch (const std::logic_error &e) {
+        std::cerr << "Logic error: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
