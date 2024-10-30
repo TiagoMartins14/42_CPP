8b7c2d67f3381a5e3b4ed1f18d70803321ad0aa2
tiaferna <tiaferna@student.42.fr>
2024-10-30 11:17:56 +0000

    improved main to force and catch a 'span full' error

--- Module_08/ex01/sources/main.cpp
+++ Module_08/ex01/sources/main.cpp
@@ -11,12 +11,25 @@ int main()
     sp.setSequence(spanArray, sizeof(spanArray) / sizeof(int));
     sp.fillSpan(); */
 
+    // Adding 5 elements
+    try {
+        sp.addNumber(6);
+        sp.addNumber(3);
+        sp.addNumber(17);
+        sp.addNumber(9);
+        sp.addNumber(11);
+    }
+    catch (const std::overflow_error &e) {
+        std::cerr << "Logic error: " << e.what() << std::endl;
+    }
 
-    sp.addNumber(6);
-    sp.addNumber(3);
-    sp.addNumber(17);
-    sp.addNumber(9);
-    sp.addNumber(11);
+    // Trying to add a sixth element
+    try {
+        sp.addNumber(20);
+    }
+     catch (const std::overflow_error &e) {
+        std::cerr << "Logic error: " << e.what() << std::endl;
+    }
 
     try {
         std::cout << sp.shortestSpan() << std::endl;
