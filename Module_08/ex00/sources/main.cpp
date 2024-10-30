0d70290282ea9d60c81f757cee28427aa1521c61
tiaferna <tiaferna@student.42.fr>
2024-10-30 14:19:29 +0000

    added a repeated number to show that it only returns the first one found each time

--- Module_08/ex00/sources/main.cpp
+++ Module_08/ex00/sources/main.cpp
@@ -5,18 +5,19 @@
 
 
 int main() {
-	int intArr[] = {1, 2, 3, 4, 5};
+	int intArr[] = {1, 2, 3, 4, 5, 5};
 	std::vector<int> intContainer(intArr, intArr + 5);
 
 	std::cout << "----- Testing with vector<int> -----" << std::endl << std::endl;
 
-	std::cout << "std::vector<int> intContainer = {1, 2, 3, 4, 5}" << std::endl << std::endl;
+	std::cout << "std::vector<int> intContainer = {1, 2, 3, 4, 5, 5}" << std::endl << std::endl;
 
 	easyfind(intContainer, 0);
 	easyfind(intContainer, 1);
 	easyfind(intContainer, 2);
 	easyfind(intContainer, 3);
 	easyfind(intContainer, 4);
+	easyfind(intContainer, 5);
 	easyfind(intContainer, 5);
 	easyfind(intContainer, 6);
 	easyfind(intContainer, -100);
