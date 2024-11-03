a1fd484703419c63bb5b551d10d0ff8bdf6bdc7d
Tiago Martins <tiago86martins@gmail.com>
2024-11-03 16:33:07 +0000

    added isValidInput() and createContainers()

--- Module_09/ex02/includes/PmergeMe.hpp
+++ Module_09/ex02/includes/PmergeMe.hpp
@@ -1,6 +1,26 @@
 #pragma once
+#include <cstdlib>
+#include <list>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
 class PmergeMe {
    private:
+	char **_argv;
+	std::vector<int> _vectorContainer;
+	std::list<int> _listContainer;
+
+	// General functions
+	bool isValidInput(const std::string &input);
+	void createContainers(const char **argv);
+
+	// Vector functions
+
+	// List functions
    public:
+	PmergeMe(const std::string &input);
+	PmergeMe(const PmergeMe &other);
+	PmergeMe &operator=(const PmergeMe &other);
+	~PmergeMe();
 };
--- Module_09/ex02/sources/PmergeMe.cpp
+++ Module_09/ex02/sources/PmergeMe.cpp
@@ -1,1 +1,24 @@
 #include "PmergeMe.hpp"
+
+#include <limits>
+
+bool PmergeMe::isValidInput(const std::string &input) {
+	for (size_t i = 0; i < input.length(); i++)
+		if (!isdigit(input[i]) || !isspace(input[i])) return false;
+
+	long number = std::atol(input.c_str());
+	if (number > std::numeric_limits<int>::max() ||
+		number < std::numeric_limits<int>::min())
+		return false;
+
+	return true;
+}
+
+void PmergeMe::createContainers(const char **argv) {
+	for (size_t i = 1; argv[i]; i++) {
+		if (!isValidInput(argv[i])) throw std::logic_error("Invalid input");
+
+		_vectorContainer.push_back(std::atoi(argv[i]));
+		_listContainer.push_back(std::atoi(argv[i]));
+	}
+}
