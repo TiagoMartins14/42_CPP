d478fbe3eb213aab06c878320909ecaee4abed03
tiaferna <tiaferna@student.42.fr>
2024-11-05 08:51:06 +0000

    updated test

--- Module_08/ex00/sources/main.cpp
+++ Module_08/ex00/sources/main.cpp
@@ -2,42 +2,43 @@
 
 #include "easyfind.hpp"
 
-
-
 int main() {
-	int intArr[] = {1, 2, 3, 4, 5, 5};
-	std::vector<int> intContainer(intArr, intArr + 5);
+  int intArr[] = {1, 2, 3, 4, 5};
+  std::vector<int> intContainer(intArr, intArr + 5);
 
-	std::cout << "----- Testing with vector<int> -----" << std::endl << std::endl;
+  std::cout << "----- Testing with vector<int> -----" << std::endl << std::endl;
 
-	std::cout << "std::vector<int> intContainer = {1, 2, 3, 4, 5, 5}" << std::endl << std::endl;
+  std::cout << "std::vector<int> intContainer = {1, 2, 3, 4, 5, 5}" << std::endl
+            << std::endl;
 
-	easyfind(intContainer, 0);
-	easyfind(intContainer, 1);
-	easyfind(intContainer, 2);
-	easyfind(intContainer, 3);
-	easyfind(intContainer, 4);
-	easyfind(intContainer, 5);
-	easyfind(intContainer, 5);
-	easyfind(intContainer, 6);
-	easyfind(intContainer, -100);
+  easyfind(intContainer, 0);
+  easyfind(intContainer, 1);
+  easyfind(intContainer, 2);
+  easyfind(intContainer, 3);
+  easyfind(intContainer, 4);
+  easyfind(intContainer, 5);
+  easyfind(intContainer, 6);
+  easyfind(intContainer, -100);
 
-	std::cout << std::endl;
+  std::cout << std::endl;
 
-	char charArr[] = {'b', 'c', 'd', 'e', 'f'};
-	std::vector<char> charContainer(charArr, charArr + 5);
+  char charArr[] = {'b', 'c', 'd', 'e', 'f'};
+  std::vector<char> charContainer(charArr, charArr + 5);
 
-	std::cout << "----- Testing with vector<char> -----" << std::endl << std::endl;
+  std::cout << "----- Testing with vector<char> -----" << std::endl
+            << std::endl;
 
-	std::cout << "std::vector<char> charContainer = {'b', 'c', 'd', 'e', 'f'}" << std::endl << std::endl;
+  std::cout << "std::vector<char> charContainer = {'b', 'c', 'd', 'e', 'f'}"
+            << std::endl
+            << std::endl;
 
-	easyfind(charContainer, 'a');
-	easyfind(charContainer, 'b');
-	easyfind(charContainer, 'c');
-	easyfind(charContainer, 'd');
-	easyfind(charContainer, 'e');
-	easyfind(charContainer, 'f');
-	easyfind(charContainer, 'g');
+  easyfind(charContainer, 'a');
+  easyfind(charContainer, 'b');
+  easyfind(charContainer, 'c');
+  easyfind(charContainer, 'd');
+  easyfind(charContainer, 'e');
+  easyfind(charContainer, 'f');
+  easyfind(charContainer, 'g');
 
-	return 0;
+  return 0;
 }
