b36b3a5dbbc225b2de93405627a7daa09ea40ee1
tiaferna <tiaferna@student.42.fr>
2024-11-05 14:10:27 +0000

    added test to check creation and orderng of pairs

--- Module_09/ex02/sources/main.cpp
+++ Module_09/ex02/sources/main.cpp
@@ -1,1 +1,17 @@
-int main() { return 0; }
+#include "PmergeMe.hpp"
+
+int main(int argc, char **argv) {
+
+  if (argc >= 2) {
+    PmergeMe algorithm(argv);
+
+    algorithm.printVector();
+    algorithm.printList();
+
+    algorithm.vectorOrderPairs();
+    algorithm.listOrderPairs();
+
+    algorithm.printVector();
+    algorithm.printList();
+  }
+}
