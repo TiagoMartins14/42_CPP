e9832ffe0e366ec0c7cc3ac887b6ead0288ad45b
tiaferna <tiaferna@student.42.fr>
2024-11-05 09:12:37 +0000

    updated main to catch eventual errors

--- Module_08/ex01/sources/main.cpp
+++ Module_08/ex01/sources/main.cpp
@@ -6,20 +6,81 @@
 
 int main() {
 
-  Span sp = Span(5);
+  // sp1 will use addNumber()
+  Span sp1 = Span(5);
+  std::cout << " ----- Span(5) ----- " << std::endl << std::endl;
 
-  sp.fillSpan(MIN, MAX);
-
-  // Trying to add MAX + 1 element
+  std::cout << "Using addNumber..." << std::endl;
   try {
-    sp.addNumber(0);
+    sp1.addNumber(1);
+    std::cout << "SUCCESS!" << std::endl;
+  } catch (const std::overflow_error &e) {
+    std::cerr << "Logic error: " << e.what() << std::endl;
+  }
+  try {
+    sp1.addNumber(2);
+    std::cout << "SUCCESS!" << std::endl;
+  } catch (const std::overflow_error &e) {
+    std::cerr << "Logic error: " << e.what() << std::endl;
+  }
+  try {
+    sp1.addNumber(3);
+    std::cout << "SUCCESS!" << std::endl;
+  } catch (const std::overflow_error &e) {
+    std::cerr << "Logic error: " << e.what() << std::endl;
+  }
+  try {
+    sp1.addNumber(4);
+    std::cout << "SUCCESS!" << std::endl;
+  } catch (const std::overflow_error &e) {
+    std::cerr << "Logic error: " << e.what() << std::endl;
+  }
+  try {
+    sp1.addNumber(5);
+    std::cout << "SUCCESS!" << std::endl;
+  } catch (const std::overflow_error &e) {
+    std::cerr << "Logic error: " << e.what() << std::endl;
+  }
+
+  // Trying to add sp.size() + 1 element
+  std::cout << "Trying to add sp.size() + 1 element... " << std::endl;
+
+  try {
+    sp1.addNumber(0);
   } catch (const std::overflow_error &e) {
     std::cerr << "Logic error: " << e.what() << std::endl;
   }
 
   try {
-    std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
+    std::cout << "Shortest span: " << sp1.shortestSpan() << std::endl;
+    std::cout << "Longest span: " << sp1.longestSpan() << std::endl;
+  } catch (const std::logic_error &e) {
+    std::cerr << "Logic error: " << e.what() << std::endl;
+  }
+
+  std::cout << std::endl << " ----- Span(MAX) ----- " << std::endl << std::endl;
+
+  // sp2 will use fillSpan()
+  Span sp2 = Span(MAX);
+
+  std::cout << "Using fillSpan()..." << std::endl;
+  try {
+    sp2.fillSpan(MIN, MAX);
+    std::cout << "SUCCESS!" << std::endl;
+  } catch (const std::overflow_error &e) {
+    std::cerr << "Logic error: " << e.what() << std::endl;
+  }
+
+  std::cout << "Trying to add sp.size() + 1 element... " << std::endl;
+  try {
+    sp1.addNumber(MAX + 1);
+  } catch (const std::overflow_error &e) {
+    std::cerr << "Logic error: " << e.what() << std::endl;
+  }
+
+  try {
+    std::cout << "Shortest span: " << sp2.shortestSpan() << std::endl;
+    std::cout << "Longest span: " << sp2.longestSpan() << std::endl;
   } catch (const std::logic_error &e) {
     std::cerr << "Logic error: " << e.what() << std::endl;
   }
