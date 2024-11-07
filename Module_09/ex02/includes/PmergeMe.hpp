0a5c416aa48d6a723c76c6ddea0cbae7ba274092
Tiago Martins <tiago86martins@gmail.com>
2024-11-07 14:47:16 +0000

    puts pairs in ascending order for both containers and splits the vector container only

--- Module_09/ex02/includes/PmergeMe.hpp
+++ Module_09/ex02/includes/PmergeMe.hpp
@@ -1,36 +1,46 @@
 #pragma once
 #include <cstdlib>
 #include <iostream>
+#include <iterator>
+#include <limits>
 #include <list>
 #include <stdexcept>
 #include <string>
 #include <vector>
 
 class PmergeMe {
-private:
-  char **_argv;
-  std::vector<int> _vectorContainer;
-  std::list<int> _listContainer;
+   private:
+	char **_argv;
+	std::vector<int> _vectorContainer;
+	std::vector<int> _vectorAidContainer;
+	std::list<int> _listContainer;
+	std::list<int> _listAidContainer;
 
-  // General functions
-  bool isValidInput(const std::string &input);
-  void createContainers(char **argv);
+	// General functions
+	bool isValidInput(const std::string &input);
+	void createContainers(char **argv);
+	long getJacobsthalNumber(size_t index);
 
-public:
-  PmergeMe(char **argv);
-  PmergeMe(const PmergeMe &other);
-  PmergeMe &operator=(const PmergeMe &other);
-  ~PmergeMe();
+   public:
+	PmergeMe(char **argv);
+	PmergeMe(const PmergeMe &other);
+	PmergeMe &operator=(const PmergeMe &other);
+	~PmergeMe();
 
-  // Vector functions
-  void vectorOrderPairs();
-  void vectorPutPairsInAscendingOrder();
-  void listPutPairsInAscendingOrder();
-  // List functions
-  void listOrderPairs();
+	// Vector functions
+	void vectorOrderPairs();
+	void vectorPutPairsInAscendingOrder();
+	void vectorSplitContainer();
+	// void vectorInsertNumbers();
 
-  // TEST FUNCTIONS
-  void printVector();
+	// List functions
+	void listOrderPairs();
+	void listPutPairsInAscendingOrder();
 
-  void printList();
+	// TEST FUNCTIONS
+	void printVector();
+	void printVectorAid();
+
+	void printList();
+	//	void printListAid();
 };
--- Module_09/ex02/sources/PmergeMe.cpp
+++ Module_09/ex02/sources/PmergeMe.cpp
@@ -1,99 +1,194 @@
 #include "PmergeMe.hpp"
 
-#include <iterator>
-#include <limits>
-
 PmergeMe::PmergeMe(char **argv) : _argv(argv) { createContainers(_argv); }
 
 PmergeMe::PmergeMe(const PmergeMe &other)
-    : _argv(other._argv), _vectorContainer(other._vectorContainer),
-      _listContainer(other._listContainer) {}
+	: _argv(other._argv),
+	  _vectorContainer(other._vectorContainer),
+	  _listContainer(other._listContainer) {}
 
 PmergeMe &PmergeMe::operator=(const PmergeMe &other) {
-  if (this != &other) {
-    _argv = other._argv;
-    _vectorContainer = other._vectorContainer;
-    _listContainer = other._listContainer;
-  }
+	if (this != &other) {
+		_argv = other._argv;
+		_vectorContainer = other._vectorContainer;
+		_listContainer = other._listContainer;
+	}
 
-  return *this;
+	return *this;
 }
 
 PmergeMe::~PmergeMe() {}
 
-void PmergeMe::createContainers(char **argv) {
-  for (size_t i = 1; argv[i]; i++) {
-    if (!isValidInput(argv[i]))
-      throw std::logic_error("Invalid input");
-
-    _vectorContainer.push_back(std::atoi(argv[i]));
-    _listContainer.push_back(std::atoi(argv[i]));
-  }
-}
-
+// General functions
 bool PmergeMe::isValidInput(const std::string &input) {
-  for (size_t i = 0; i < input.length(); i++)
-    if (!isdigit(input[i]) && !isspace(input[i]))
-      return false;
+	for (size_t i = 0; i < input.length(); i++)
+		if (!isdigit(input[i]) && !isspace(input[i])) return false;
 
-  long number = std::atol(input.c_str());
-  if (number > std::numeric_limits<int>::max() ||
-      number < std::numeric_limits<int>::min())
-    return false;
+	long number = std::atol(input.c_str());
+	if (number > std::numeric_limits<int>::max() ||
+		number < std::numeric_limits<int>::min())
+		return false;
 
-  return true;
+	return true;
 }
 
+void PmergeMe::createContainers(char **argv) {
+	for (size_t i = 1; argv[i]; i++) {
+		if (!isValidInput(argv[i])) throw std::logic_error("Invalid input");
+
+		_vectorContainer.push_back(std::atoi(argv[i]));
+		_listContainer.push_back(std::atoi(argv[i]));
+	}
+}
+
+long PmergeMe::getJacobsthalNumber(size_t index) {
+	long a = 0;
+	long b = 1;
+	long number = 0;
+
+	while (index != 0) {
+		number = b + (2 * a);
+		a = b;
+		b = number;
+		index--;
+	}
+
+	return number;
+}
+
+// Vector functions
 void PmergeMe::vectorOrderPairs() {
-  for (size_t i = 0; i < _vectorContainer.size(); i += 2) {
-    if (i < _vectorContainer.size() - 1) {
-      if (_vectorContainer[i] > _vectorContainer[i + 1]) {
-        int temp = _vectorContainer[i];
-        _vectorContainer[i] = _vectorContainer[i + 1];
-        _vectorContainer[i + 1] = temp;
-      }
-    }
-  }
+	for (size_t i = 0; i < _vectorContainer.size(); i += 2) {
+		if (i < _vectorContainer.size() - 1) {
+			if (_vectorContainer[i] > _vectorContainer[i + 1]) {
+				int temp = _vectorContainer[i];
+				_vectorContainer[i] = _vectorContainer[i + 1];
+				_vectorContainer[i + 1] = temp;
+			}
+		}
+	}
 }
 
+void PmergeMe::vectorPutPairsInAscendingOrder() {
+	size_t i = 0;
+	size_t stop;
+	if (_vectorContainer.size() % 2 == 0)
+		stop = _vectorContainer.size() - 2;
+	else
+		stop = _vectorContainer.size() - 3;
+
+	while (i != stop) {
+		if (_vectorContainer[i + 1] > _vectorContainer[i + 3]) {
+			int mainChainValue = _vectorContainer[i + 1];
+			int secondaryChainValue = _vectorContainer[i];
+			_vectorContainer[i] = _vectorContainer[i + 2];
+			_vectorContainer[i + 2] = secondaryChainValue;
+			_vectorContainer[i + 1] = _vectorContainer[i + 3];
+			_vectorContainer[i + 3] = mainChainValue;
+			i = 0;
+		} else
+			i += 2;
+	}
+}
+
+void PmergeMe::vectorSplitContainer() {
+	std::vector<int>::iterator it = _vectorContainer.begin();
+
+	for (size_t i = 0; i < _vectorContainer.size(); i++) {
+		if (i == _vectorContainer.size() - 1) break;
+		_vectorAidContainer.push_back(_vectorContainer[i]);
+		_vectorContainer.erase(it);
+		it++;
+	}
+}
+
+// List functions
 void PmergeMe::listOrderPairs() {
-  std::list<int>::iterator it;
-  for (it = _listContainer.begin(); it != _listContainer.end();) {
-    int num1 = *it;
-    it++;
-    if (it != _listContainer.end()) {
-      int num2 = *it;
-      if (num1 > num2) {
-        *it = num1;
-        it--;
-        *it = num2;
-        it++;
-      }
-      it++;
-    }
-  }
+	std::list<int>::iterator it;
+	for (it = _listContainer.begin(); it != _listContainer.end();) {
+		int num1 = *it;
+		it++;
+		if (it != _listContainer.end()) {
+			int num2 = *it;
+			if (num1 > num2) {
+				*it = num1;
+				it--;
+				*it = num2;
+				it++;
+			}
+			it++;
+		}
+	}
 }
 
-void PmergeMe::vectorPutPairsInAscendingOrder() {}
+void PmergeMe::listPutPairsInAscendingOrder() {
+	if (_listContainer.size() < 4) return;
 
-void PmergeMe::listPutPairsInAscendingOrder() {}
+	std::list<int>::iterator it = _listContainer.begin();
+	std::list<int>::iterator stopIt = _listContainer.end();
+	if (_listContainer.size() % 2 == 0)
+		std::advance(stopIt, -2);
+	else
+		std::advance(stopIt, -3);
+
+	while (it != stopIt) {
+		int secondaryChainNum1 = *it;
+		it++;
+		int mainChainNum1 = *it;
+		it++;
+		int secondaryChainNum2 = *it;
+		it++;
+		int mainChainNum2 = *it;
+		if (mainChainNum1 > mainChainNum2) {
+			*it = mainChainNum1;
+			it--;
+			*it = secondaryChainNum1;
+			it--;
+			*it = mainChainNum2;
+			it--;
+			*it = secondaryChainNum2;
+			it = _listContainer.begin();
+		} else {
+			it--;
+		}
+	}
+}
 
 // TEST FUNCTIONS
 void PmergeMe::printVector() {
-  std::cout << "Elements in vector: ";
-  for (size_t i = 0; i < _vectorContainer.size(); ++i) {
-    std::cout << _vectorContainer[i] << " ";
-  }
-  std::cout << std::endl;
+	std::cout << "Elements in vector: ";
+	for (size_t i = 0; i < _vectorContainer.size(); ++i) {
+		if (i % 2 == 1)
+			std::cout << "[" << _vectorContainer[i] << "] ";
+		else
+			std::cout << _vectorContainer[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+void PmergeMe::printVectorAid() {
+	std::cout << "Elements in vectorAid: ";
+	for (size_t i = 0; i < _vectorAidContainer.size(); ++i) {
+		if (i % 2 == 1)
+			std::cout << "[" << _vectorAidContainer[i] << "] ";
+		else
+			std::cout << _vectorAidContainer[i] << " ";
+	}
+	std::cout << std::endl;
 }
 
 void PmergeMe::printList() {
-  std::cout << "Elements in list: ";
-  for (std::list<int>::const_iterator it = _listContainer.begin();
-       it != _listContainer.end(); ++it) {
-    std::cout << *it << " ";
-  }
-  std::cout << std::endl;
+	int index = 0;
+	std::cout << "Elements in list:   ";
+	for (std::list<int>::const_iterator it = _listContainer.begin();
+		 it != _listContainer.end(); ++it) {
+		if (index % 2 == 1)
+			std::cout << "[" << *it << "] ";
+		else
+			std::cout << *it << " ";
+		index++;
+	}
+	std::cout << std::endl;
 }
 
-// TESTE FUNCTIONS
+// TEST FUNCTIONS
