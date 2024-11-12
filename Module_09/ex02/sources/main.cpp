42442d942d5ce4aa5714180eae8fe50b0339b4eb
Tiago Martins <tiago86martins@gmail.com>
2024-11-12 14:53:19 +0000

    cleared the test to only display sorted containers and catch any errors

--- Module_09/ex02/sources/main.cpp
+++ Module_09/ex02/sources/main.cpp
@@ -1,44 +1,24 @@
 #include <cmath>
+#include <exception>
 
 #include "PmergeMe.hpp"
 
 int main(int argc, char **argv) {
-	if (argc >= 2) {
-		PmergeMe algorithm(argv);
-		std::cout << " ----- Original containers ----- " << std::endl;
-		algorithm.printVector();
-		algorithm.printList();
-		std::cout << std::endl;
-
-		algorithm.vectorOrderPairs();
-		algorithm.listOrderPairs();
-
-		std::cout << " ----- Ordered pairs ----- " << std::endl;
-		algorithm.printVector();
-		algorithm.printList();
-		std::cout << std::endl;
-
-		algorithm.vectorPutPairsInAscendingOrder();
-		algorithm.listPutPairsInAscendingOrder();
-
-		std::cout << " ----- Pairs in ascending order ----- " << std::endl;
-		algorithm.printVector();
-		algorithm.printList();
-		std::cout << std::endl;
-
-		algorithm.vectorSplitContainer();
-		algorithm.listSplitContainer();
-
-		std::cout << " ----- Split containers ----- " << std::endl;
-		algorithm.printVector();
-		algorithm.printVectorAid();
-		std::cout << std::endl;
-		algorithm.printList();
-		algorithm.printListAid();
-		std::cout << std::endl;
-
-		std::cout << " ----- Sort containers ----- " << std::endl;
-		algorithm.vectorInsertNumbers();
-		algorithm.printVector();
+	if (argc <= 2) {
+		std::cerr << "Invalid input! Valid input: "
+					 "./PmergeMe [at least two positive "
+					 "integers]"
+				  << std::endl;
+		return 1;
 	}
+
+	try {
+		PmergeMe algorithm(argv);
+		algorithm.sortVector();
+		algorithm.sortList();
+	} catch (const std::exception &e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
+
+	return 0;
 }
